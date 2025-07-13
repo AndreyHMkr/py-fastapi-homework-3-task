833834126988fee75a43f8040ab6c47335b16b42
Andriy <grushevoyandrey@gmail.com>
2025-07-13 16:42:50 +0100

    add schemas for user registration, login, activation, and token refresh

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,63 @@
 from pydantic import BaseModel, EmailStr, field_validator
 
-from database import accounts_validators
+from database.validators.accounts import validate_email, validate_password_strength
 
 
-# Write your code here
+class UserBase(BaseModel):
+    email: EmailStr
+
+    @field_validator("email")
+    @classmethod
+    def email_validate(cls, value: EmailStr) -> EmailStr:
+        return validate_email(value)
+
+
+class UserRegistrationRequestSchema(UserBase):
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def password_validate(cls, value: str) -> str:
+        return validate_password_strength(value)
+
+
+class UserRegistrationResponseSchema(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str = "User account activated successfully."
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(UserRegistrationRequestSchema):
+    token: str
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class UserLoginRequestSchema(UserRegistrationRequestSchema):
+    pass
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
